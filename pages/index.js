c4124e6c49ab0bf6afeff5f66b8f3b7652c6be4b
Alejandro Licea <alicea@foundationmedicine.com>
2020-10-20 23:33:25 -0500

    added functionality for 'load more' button

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import SearchBar from '../components/SearchBar'
 import CardsContainer from '../components/CardsContainer'
 import { fetchList } from '../sources/rentals'
+import Button from '../components/Button'
 
 export default function Home () {
   const [list, setList] = useState([])
@@ -18,6 +19,13 @@ export default function Home () {
       .catch(err => console.error('error fetching rental list: ', err))
   }, [])
 
+  const handleOnClick = () => {
+    fetchOptions['page[limit]'] += 8
+    fetchList(fetchOptions)
+      .then(({ data: { data } }) => setList(data))
+      .catch(err => console.error('error fetching rental list: ', err))
+  }
+
   return (
     <div className='max-w-screen-xl mx-auto mt-8'>
       <h1 className='text-5xl font-black text-gray-600'>Campervans</h1>
@@ -26,6 +34,15 @@ export default function Home () {
       </section>
       <section>
         {list && <CardsContainer list={list} />}
+      </section>
+      <section>
+        <Button
+          name='loadMore'
+          onClick={handleOnClick}
+          className=''
+        >
+          Load More
+        </Button>
       </section>
     </div>
   )
